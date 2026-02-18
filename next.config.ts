6976fadb289670e7f30a627e65cdf29ad00e6517
Lucas Alves <lucas.nascimento.al16@gmail.com>
2026-02-18 19:58:53 -0300

    novas alterações

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,29 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '8085',
+        pathname: '/uploads/**',
+      },
+    ],
+  },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:8085/api/:path*',
+      },
+      {
+        source: '/uploads/:path*',
+        destination: 'http://localhost:8085/uploads/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
